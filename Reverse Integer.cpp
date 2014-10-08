6823beb1c774fb49e25765faf413be6979fd7dfa
zhouyoulie <zhouyouisme@163.com>
2014-10-08 15:22:14 +0800

    Reverse Integer
    Reverse Integer

---
+++ Reverse Integer.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int reverse(int x){
+    bool ispositive = x>=0?true:false;
+    int data_ret = 0;
+
+    while(x){
+        data_ret *= 10;
+        int add = x % 10;
+
+        if((ispositive&&(INT_MAX -data_ret)>=add) ||
+           (!ispositive&&(INT_MIN -data_ret)<=add))
+            data_ret += add;
+        else return -1;//overflow happens
+
+        x /= 10;
+    }
+
+    return data_ret;
+}
+    
+    
+};
